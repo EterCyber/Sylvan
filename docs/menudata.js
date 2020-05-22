8613a6a3c4c40492d07eacfffb90af21721083bb
Wilbert Lee <wilbert-lee@outlook.com>
2020-05-22 15:40:51 +0800

    Add files via upload

--- docs/menudata.js
+++ docs/menudata.js
@@ -21,7 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
+{text:"Home Page",url:"index.html"},
 {text:"Namespaces",url:"namespaces.html",children:[
 {text:"Namespace List",url:"namespaces.html"},
 {text:"Namespace Members",url:"namespacemembers.html",children:[
